58725884c65df973fb621011a19f9012dca2130f
Pedro Henrique Iwamoto <phiwamoto@hotmail.com>
2019-03-11 20:12:58 -0300

    Operação de consulta - Teste com o Postman

--- index.js
+++ index.js
@@ -19,11 +19,16 @@ server.use(restify.plugins.acceptParser(server.acceptable));
 server.use(restify.plugins.queryParser());
 server.use(restify.plugins.bodyParser());
 
-server.get('/echo/:name', function (req, res, next) {
-  res.send(req.params);
-  return next();
-});
-
 server.listen(8080, function () {
   console.log('%s listening at %s', server.name, server.url);
 });
+
+server.get('/', function (req, res, next) {
+
+    knex('rest').then((dados)=>{
+        res.send(dados);
+    }, next);
+
+    return next();
+  });
+  
